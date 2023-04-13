80699331fc3d131169aaec9631439336afa08a5c
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-04-13 05:28:34 -0500

    Updated login button

--- docs/components/Button.js
+++ docs/components/Button.js
@@ -1,5 +1,5 @@
 export const Button = ({ text = 'Get Started' }) => (
-  <a href='/login'>
+  <a href='https://oauth.do/login'>
     <div type='button' className='rounded-md bg-blue-600 ml-2 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
       {text}
     </div>
