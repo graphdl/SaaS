af8192747955fc1549d62603fe1fca47669c75e4
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-04-13 05:27:27 -0500

    Added login button

--- docs/components/Button.js
+++ docs/components/Button.js
@@ -1,5 +1,7 @@
 export const Button = ({ text = 'Get Started' }) => (
-  <button type='button' className='rounded-md bg-blue-600 ml-2 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
-    {text}
-  </button>
+  <a href='/login'>
+    <div type='button' className='rounded-md bg-blue-600 ml-2 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
+      {text}
+    </div>
+  </a>
 )
